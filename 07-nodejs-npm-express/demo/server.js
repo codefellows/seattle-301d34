2ceb109e3105357eb7a823da8fcedc91739f3c97
Allie <allie@codefellows.com>
2018-05-15 13:28:24 -0700

    adding demo before class

---
+++ 07-nodejs-npm-express/demo/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Load the express library from node_modules
+// Returns a function reference
+
+// Instantiate express so we can use its functionality
+// Now the constant variable "app" is an object and we can use its methods
+// We are going to use methods for routing HTTP requests, which come from the view
+
+// Designate a port to serve our app on
+// "process" is in the Node environment, use a port if it is set up, or set your own
+
+// To set up your own PORT in your terminal type: export PORT=3000
+
+// Tell the server which directory to serve files from
+
+// Set up a route to send a message
+
+// Set up a route to send a file
+// Include the root for relative file paths
+
+// Can we perform other methods on the same route?
+
+// Start the app so it listens for changes
